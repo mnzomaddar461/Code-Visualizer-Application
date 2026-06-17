9d4be09af7b037fcda6d24120d23bfec8ee84022
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2026-06-18 01:15:22 +0600

    Formet error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import { ThemeProvider } from './components/ThemeContext'
+import { ThemeProvider } from './components/Themecontext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
